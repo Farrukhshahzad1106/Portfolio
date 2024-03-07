152d8823ae167593a0391f26a1f01a824677e50a
Farrukh <shahzadfarrukh621@gmail.com>
2024-03-07 17:08:36 +0530

    Made a folder structure of the project and laid out a future plan of action

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,4 +18,9 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
+
+
+// When the user at the top of the page then the navbar will appear in full length having the entire viewport width.
+// When the user scrolls down or navigated to anywhere from the navbar then the navbar component will be a floating but fixed at a distance from the top of the viewport overlapping the main content with a slightly different background and border radius set to a vakue which will appear slightly rounded.
+// When the user again scrolls to the top it will again aquire full viewport width.
