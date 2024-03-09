3df2b05ffd1c9ead47139ee3fa1a9e0887f43039
Farrukh <shahzadfarrukh621@gmail.com>
2024-03-09 16:03:05 +0530

    MAke a structural layout of how the contact page should look like, designing part remaining

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,8 +1,42 @@
 import React from 'react'
 
 const ContactMe = () => {
+  let intro =  `Let's Chat.
+                Tell me about your 
+                Project`
   return (
-    <div>ContactMe</div>
+    <div>
+      <div>
+        <h2>
+          Let's Chat.<br/>
+          Tell me about your <br/>
+          Project
+        </h2>
+        <p>Let's create something together.</p>
+        <p>
+          Mail me at <br/>
+          <span id='email'> shahzadfarrukh621@gmail.com </span>
+        </p>
+      </div>
+      <div>
+        <h1>
+          Send us a message
+        </h1>
+        <form>
+          <input/>
+          <input/>
+          <input/>
+          <textarea>
+
+          </textarea>
+          <button>
+            Send Message
+          </button>
+        </form>
+      </div>
+      
+    </div>
+    
   )
 }
 
